aa11526cc17ec22e4ac7a9dc9d34b0e28501d6b4
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-23 12:16:00 -0300

    feat: add forecast service

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -56,7 +56,7 @@ export class StormGlass {
 
   constructor(protected request = new HTTPUtil.Request()) {}
 
-  public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+  public async fetchPoints(lat: Number, lng: Number): Promise<ForecastPoint[]> {
     try {
       const response = await this.request.get<StormGlassForecastResponse>(
         `${stormGlassResourceConfig.get(
